60e66f60bf1be41b4f2e2f1e9e7967b6426e73e7
Jordanpatton78 <jordantreypatton@gmail.com>
2023-08-09 17:44:35 -0600

    Made changes to webScraper

--- webScrape.py
+++ webScrape.py
@@ -1,15 +1,40 @@
-import pytesseract
-from PIL import Image
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup as bs
 import re as re
+import time
+import pandas as pd
+import getpass
 
-# Load the screenshot image
-screenshot_path = 'jordanPatton.png'  # Update this with your screenshot file path
-image = Image.open(screenshot_path)
+PATH = '/mnt/chromeos/MyFiles/Downloads/chromedriver_linux64'
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.binary_location = "/usr/share/bash-completion/completions/google-chrome"  # Set the binary location
+# chrome_options.add_argument("--headless")  # Add any desired arguments
+USERNAME = input("Enter the username: ")
+PASSWORD = getpass.getpass(prompt="Enter the password: ")
+passwordLen = len(PASSWORD)
+print(PATH)
+print(USERNAME)
+i=0
+password = []
+while i < passwordLen:
+    password.append('*')
+    i+=1
+password = "".join(password)
+print(password)
 
-# Use Tesseract to extract text from the image
-text = pytesseract.image_to_string(image)
-nameObject = re.search(r'([\w\s]{1,20})\|[\w\s]{1,20}',text)
-name = nameObject.group(1)
+driver = webdriver.Chrome()
+driver.get("https://www.linkedin.com/uas/login")
+time.sleep(3)
 
-# Print the extracted text
-print(name)
+email=driver.find_element(By.ID, "username")
+email.send_keys(USERNAME)
+password=driver.find_element(By.ID, "password")
+password.send_keys(PASSWORD)
+time.sleep(3)
+password.send_keys(Keys.RETURN)
